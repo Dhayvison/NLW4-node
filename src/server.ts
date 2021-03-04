28dccb5ddfb762898dd838145d8620a4061ead1b
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-03 21:40:53 -0300

    feat: add dotenv and cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
-import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import express, { Response } from 'express';
 import { success } from './utils/text-coloring';
 
 const app = express();
+app.use(cors());
+app.disable('x-powered-by');
 
-app.listen(3333, () => console.info(success`Server is running`));
+app.get('/', (req, res: Response) => {
+  res.json({ status: 'running' });
+});
+
+app.listen(process.env.PORT ?? 3333, () =>
+  console.info(success`Server is running on port:`, process.env.PORT ?? 3333),
+);
