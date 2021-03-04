7654cf172347e85fb5fd867576a52e9f61fc6b14
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-04 01:37:23 -0300

    feat: add read, update and delete to User

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -16,6 +16,58 @@ class UserController {
     const saved = await newUser.save();
     return response.status(201).json({ user: saved });
   }
+
+  async read(request: Request, response: Response) {
+    const { id } = request.params;
+    if (id) {
+      try {
+        const user = await User.findOneOrFail({ id });
+        return response.json(user);
+      } catch (error) {
+        return response.status(404).json({
+          error: 'User not found',
+        });
+      }
+    } else {
+      return response.json(await User.find());
+    }
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    try {
+      const user = await User.findOneOrFail({ id });
+
+      if (name && email) {
+        user.name = name;
+        user.email = email;
+        const updated = await user.save();
+        return response.json(updated);
+      } else {
+        return response.status(400).json({ error: 'Empty name or email' });
+      }
+    } catch (error) {
+      return response.status(404).json({
+        error: 'User not found',
+      });
+    }
+  }
+
+  async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      const user = await User.findOneOrFail({ id });
+      const deleted = await user.remove();
+      return response.json(deleted);
+    } catch (error) {
+      return response.status(404).json({
+        error: 'User not found',
+      });
+    }
+  }
 }
 
 export { UserController };
--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,8 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/users', userController.create);
+router.get('/users/:id?', userController.read);
+router.put('/users/:id', userController.update);
+router.delete('/users/:id', userController.delete);
 
 export { router };
