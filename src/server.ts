5c97b17ab0e6cf6b81d3b9bf8a341bf03080cd6a
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-03 22:17:14 -0300

    feat: add typeorm and sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config';
+import 'reflect-metadata';
 import cors from 'cors';
-import express, { Response } from 'express';
+import express from 'express';
 import { success } from './utils/text-coloring';
 
 const app = express();
 app.use(cors());
 app.disable('x-powered-by');
 
-app.get('/', (req, res: Response) => {
-  res.json({ status: 'running' });
+app.get('/', (...[, response]) => {
+  response.json({ status: 'running' });
 });
 
 app.listen(process.env.PORT ?? 3333, () =>
