b02cc4c6ae76368cc6c428b61019092e138d77e9
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-03 23:35:38 -0300

    feat: create base to user controller and routes

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,19 @@ import 'reflect-metadata';
 import cors from 'cors';
 import express from 'express';
 import { success } from './utils/text-coloring';
+import { router } from './routes';
 import './database';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.disable('x-powered-by');
 
 app.get('/', (...[, response]) => {
-  response.json({ status: 'running' });
+  return response.json({ status: 'running' });
 });
+
+app.use(router);
 
 app.listen(process.env.PORT ?? 3333, () =>
   console.info(success`Server is running on port:`, process.env.PORT ?? 3333),
