62af34dc6301b01b404ced2f76f00d819dd8461d
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-06 00:12:24 -0300

    chore: apply test database config to test enviorment

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,14 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+(async () => {
+  const defaultOptions = await getConnectionOptions();
+  const isTestEnv = process.env.NODE_ENV === 'test';
+
+  createConnection(
+    Object.assign(defaultOptions, {
+      ...(isTestEnv && {
+        database: './src/database/database.test.sqlite',
+      }),
+    }),
+  );
+})();
