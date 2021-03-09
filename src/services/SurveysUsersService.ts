41e7f131c14dcbdc18a6bcb110615231f4bc5a36
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-08 22:39:15 -0300

    feat: add method check if survey has been sent

--- src/services/SurveysUsersService.ts
+++ src/services/SurveysUsersService.ts
@@ -6,6 +6,18 @@ class SurveysUsersService {
     const created = await newSurvey.save();
     return { error: '', created };
   }
+
+  static async alreadyExists(userId: string, surveyId: string) {
+    const surveyUser = await SurveysUsers.findOne({
+      user_id: userId,
+      survey_id: surveyId,
+    });
+    if (!surveyUser) {
+      return { error: '' };
+    } else {
+      return { error: 'This survey has already been sent to this user' };
+    }
+  }
 }
 
 export { SurveysUsersService };
