acb4e249260f6d33884cdb364836c0fc9aa2b32f
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-10 07:53:15 -0300

    feat: add config to production enviorment

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,14 +3,27 @@ import { createConnection, getConnectionOptions } from 'typeorm';
 export default (async () => {
   const defaultOptions = await getConnectionOptions();
   const isTestEnv = process.env.NODE_ENV === 'test';
+  const isProdEnv = process.env.NODE_ENV === 'production';
 
   const database = await createConnection(
     Object.assign(defaultOptions, {
       ...(isTestEnv && {
         database: './src/database/database.test.sqlite',
       }),
+      ...(isProdEnv && {
+        database: './dist/src/database/database.test.sqlite',
+        entities: ['./dist/src/models/**.js'],
+        migrations: ['./dist/src/database/migrations/**.js'],
+        cli: {
+          migrationsDir: './dist/src/database/migrations',
+        },
+      }),
     }),
   );
+
+  if (isProdEnv) {
+    await database.runMigrations();
+  }
 
   return database;
 })();
