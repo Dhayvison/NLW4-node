784b85edd84e632b2d992cac018efd5f3cb1730f
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2021-03-10 08:20:05 -0300

    fix: port bind

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import { success } from './utils/text-coloring';
 
-app.listen(process.env.PORT ?? 3333, () =>
-  console.info(success`Server is running on port:`, process.env.PORT ?? 3333),
+app.listen(process.env.PORT || 3333, () =>
+  console.info(success`Server is running on port:`, process.env.PORT || 3333),
 );
